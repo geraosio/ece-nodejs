792825fe0de3ad4ff56014cc5f61fc50c1a543f3
geraosio <geraosio@gmail.com>
2019-07-11 16:31:15 +0200

    Add instructions in the rooth path and 404 when trying to access a page not defined

--- as_module/handles.js
+++ as_module/handles.js
@@ -9,10 +9,19 @@ module.exports = {
 
     res.writeHead(200, {'Content-Type': 'text/plain'});
 
-    if (path === '/hello' && 'name' in params) {
-      res.write('Hello ' + params['name'])
+    
+    if (path === '/') {
+      res.write('This app says hello, and if you tell it your name it sends a personalized message.\n\n\n')
+      res.write('To begin go to the path /hello?name=YourName.\n')
+      res.write('If you want to use spaces in your name you need to use %20 instead of the whitespace. For example name=Your%20Name.\n')
+    } else if (path === '/hello' && 'name' in params) {
+      if (params['name'] === 'Gerardo') {
+        res.write("Hello my name is Gerardo Osio. I'm studying at ECE!")
+      } else {
+        res.write('Hello ' + params['name'])
+      }
     } else {
-      res.write('Hello anonymous')
+      res.write('404 Page not found')
     }
     
     res.end();
