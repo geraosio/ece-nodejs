5a88d07416a6ea0e4002235ec1cf09e1c850c679
geraosio <geraosio@gmail.com>
2019-07-15 12:59:29 +0200

    Convert app to use Express

--- index.js
+++ index.js
@@ -1,6 +1,28 @@
-// ./index.js
-const http = require('http')
-const handles = require('./handles')
+const express = require('express')
 
-const server = http.createServer(handles.serverHandle);
-server.listen(8080)
+const app = express()
+const port = 8080
+
+app.listen(port,
+  () => console.log(`server listening on ${port}!`)
+)
+
+app.get('/', function(req, res) {
+  res.send(`
+    This app says hello, and if you tell it your name it sends a personalized message.\n
+    To begin go to the path /hello/YourName.\n
+    If you want to use spaces in your name you need to use %20 instead of the whitespace. For example to write John Doe you need to put John%20Doe.
+  `)
+})
+
+app.get('/hello/:name', function(req, res) {
+  if (req.params.name == "Gerardo") {
+    res.send("Hello my name is Gerardo Osio. I'm studying at ECE!")
+  } else {
+    res.send('Hello ' + req.params.name)
+  }
+})
+
+app.use(function (res, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
