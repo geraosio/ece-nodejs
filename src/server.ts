d480efa96891f89003b18cb1e5bf6c427d52a776
geraosio <geraosio@gmail.com>
2019-07-16 14:39:17 +0200

    Initialize db connection from the main app

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,43 @@
 import express = require('express')
 import { MetricsHandler } from './metrics'
+import mongodb from 'mongodb'
 
 const path = require('path')
 const app = express()
-app.use(express.static(path.join(__dirname, 'public')))
 const port: string = process.env.PORT || '8080'
+const MongoClient = mongodb.MongoClient // Create a new MongoClient
+
+//
+// DB
+//
+
+var db: any
+
+MongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true}, (err: any, client: any) => {
+  if (err) throw err
+  db = client.db('mydb')
+  
+  // Start the application after the databse connection is ready
+  const port: string = process.env.PORT || '8115'
+  app.listen(port, (err: Error) => {
+    if (err) throw err
+    console.log(`server is listening on port ${port}`)
+  })
+})
+
+// APP USE & SET
+
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.set('views', __dirname + "/views")
 app.set('view engine', 'ejs')
 
+//
+// GET
+//
+
 app.get('/', (req: any, res: any) => {
-  // res.write('Hello World')
   res.render('pages/index')
-  // res.end()
 })
 
 app.get('/metrics', (req: any, res: any) => {
@@ -32,6 +57,10 @@ app.get('/hello/:name', (req:any, res:any) => {
   }
 })
 
+//
+// ERRORS
+//
+
 app.use(function (req: any, res: any, next: any) {
   res.status(404).send("Sorry can't find that!")
 })
@@ -40,6 +69,10 @@ app.use(function (err: any, req: any, res: any, next: any) {
   console.error(err.stack);
   res.status(500).send("Something broke!")
 })
+
+//
+// LISTEN
+//
 
 app.listen(port, (err: Error) => {
   if (err) {
