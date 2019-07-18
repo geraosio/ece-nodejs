e89882b3a7232bdf30d2dc1077dea4590be3d107
geraosio <geraosio@gmail.com>
2019-07-18 15:22:17 +0200

    Add test as it was given, test pending

--- src/tests/metrics.test.ts
+++ src/tests/metrics.test.ts
@@ -1,9 +1,42 @@
-import {expect} from 'chai'
+import { expect } from 'chai'
+import { Metric, MetricsHandler } from '../metrics'
+import mongodb from 'mongodb'
 
-const a: number = 0
+var dbMet: MetricsHandler
+var db: any
+var clientDb: any
 
-describe('Metrics', function() {
-  it('should save and get', function() {
-    expect(a).to.equal(0)
+var mongoAsync = (callback: any) => {
+  const MongoClient = mongodb.MongoClient // Create a new MongoClient
+  MongoClient.connect("mongodb://localhost:27017", { useNewUrlParser: true }, (err: any, client: any) => {
+    if(err) throw err
+    callback(client)
+  });
+}
+
+describe('Metrics', () => {
+  before((done) =>  {
+    mongoAsync((client: any) => {
+      clientDb = client
+      db = clientDb.db('mydb')
+      dbMet = new MetricsHandler(db)
+      done()
+    })
+  })
+
+  after(() => {
+    clientDb.close()
+  })
+
+  // ...
+})
+
+describe('/get', () =>  {
+  it('should get empty array', function() {
+    dbMet.get(function(err: Error | null, result?: Metric[]) {
+      expect(err).to.be.null
+      expect(result).to.not.be.undefined
+      expect(result).to.be.empty
+    })
   })
 })
